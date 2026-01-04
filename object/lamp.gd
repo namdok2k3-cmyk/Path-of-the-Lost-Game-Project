08bb4c8538b9ee8965e159da0dc2ce4c256c67a5
namdok2k3-cmyk <namdok2k3@gmail.com>
2026-01-04 03:26:33 +0000

    Deploying to gh-pages from @ namdok2k3-cmyk/Path-of-the-Lost-Game-Project@4a4396c0a444a565831722b51238822ad5821d46 🚀

--- object/lamp.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@export var light_scale = 2
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$object/PointLight2D.texture_scale = light_scale 
-	$object/PointLight2D.texture.width = light_scale * 64
-	$object/PointLight2D.texture.height = light_scale * 64
